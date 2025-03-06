741d39091aaea43fcce53a0550fdaa3a7be2b8e8
Ishan Pradhan <ishanpradhan72@gmail.com>
2025-03-06 14:01:30 +0545

    add client-side rendering with React and pizza menu using hydrate

--- Manual-SSR/client.js
+++ Manual-SSR/client.js
@@ -1,8 +1,5 @@
-const { readFileSync } = require("fs");
-const { createServer } = require("http");
-const { parse } = require("url");
-const { renderToString } = require("react-dom/server");
-const React = require("react");
+ReactDOM.hydrateRoot(document.getElementById("root"), <Home />);
+
 const pizzas = [
   {
     name: "Focaccia",
@@ -62,26 +59,3 @@ function MenuItem({ pizza }) {
     </li>
   );
 }
-
-const htmlTemplate = readFileSync(`${__dirname}/index.html`, "utf-8");
-const clientJS = readFileSync(`${__dirname}/client.js`, "utf-8");
-
-const server = createServer((req, res) => {
-  const pathName = parse(req.url, true).pathname;
-
-  console.log(pathName);
-  if (pathName === "/") {
-    const renderedReact = renderToString(<Home />);
-    const html = htmlTemplate.replace("%%%CONTENT%%%", renderedReact);
-    res.end(html);
-  } else if (pathName === "/client.js") {
-    res.writeHead(200, { "Content-Type": "text/javascript" });
-    res.end(clientJS);
-  } else {
-    res.end("OH NO!The url can't be found");
-  }
-});
-
-server.listen(8000, () => {
-  console.log("listening for requests for port 8000");
-});
