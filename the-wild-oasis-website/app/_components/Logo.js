b811b8f7795aad23cb18c725aaefcb98f2fb36aa
Ishan Pradhan <ishanpradhan72@gmail.com>
2025-03-19 17:00:59 +0545

    feat: update Logo component to use Next.js Image and enhance page layout

--- the-wild-oasis-website/app/_components/Logo.js
+++ the-wild-oasis-website/app/_components/Logo.js
@@ -1,9 +1,22 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      {/* Note : need to specify height and width here */}
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
+
+      {/* if we use import then we don't need to specify height it won't give error */}
+      {/* NOTE: quality changes the file size of the image */}
+      <Image
+        src={logo}
+        height="60"
+        width="60"
+        quality={100}
+        alt="The Wild Oasis logo"
+      />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
--- the-wild-oasis-website/app/page.js
+++ the-wild-oasis-website/app/page.js
@@ -2,10 +2,20 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div>
-      <h1>The Wild Oasis. Welcome to paradise.</h1>
+    <main className="mt-24">
+      <img src="/bg.png" alt="Mountains and forests with two cabins" />
 
-      <Link href="/cabins">Explore luxury cabins.</Link>
-    </div>
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <a
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </a>
+      </div>
+    </main>
   );
 }
