5f25da1253e10f5a7b013b2df900f71b74125a23
Ishan Pradhan <ishanpradhan72@gmail.com>
2025-03-22 20:22:31 +0545

    refactor: remove commented title from layout metadata

--- the-wild-oasis-website/app/layout.js
+++ the-wild-oasis-website/app/layout.js
@@ -9,7 +9,6 @@ const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 console.log(josefin);
 
 export const metadata = {
-  // title: "The Wild Oasis",
   title: {
     template: "%s - The Wild Oasis",
     default: "Welcome - The Wild Oasis",
