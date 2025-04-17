13a952d72b8941e0b7b0338e3aa7b404c62fa986
Ishan Pradhan <ishanpradhan72@gmail.com>
2025-04-17 20:32:27 +0545

    nested routes and layout

--- the-wild-oasis-website/app/layout.js
+++ the-wild-oasis-website/app/layout.js
@@ -24,7 +24,9 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen antialiased flex flex-col relative`}
       >
         <Header />
-        <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
