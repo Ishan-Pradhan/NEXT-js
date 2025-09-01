b597b0541df56aaced026f86a61ae2751926f782
Ishan Pradhan <ishanpradhan72@gmail.com>
2025-09-01 12:00:18 +0545

    refactor: clean up CabinList component and update About page to include total cabins

--- the-wild-oasis-website/app/_components/CabinList.js
+++ the-wild-oasis-website/app/_components/CabinList.js
@@ -1,7 +1,9 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
+// import { unstable_noStore as noStore } from "next/cache";
 
 const CabinList = async () => {
+  // noStore();
   const cabins = await getCabins();
   if (!cabins || cabins.length === 0) {
     return null;
